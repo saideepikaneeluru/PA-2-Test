3493cb4ca5ea7fe28c2d1db0f6d13b1dfeeaea50
saideepikaneeluru <158000517+saideepikaneeluru@users.noreply.github.com>
2024-03-27 14:08:21 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,4 +1,3 @@
-import myplots as myplt
 import time
 import warnings
 import numpy as np
@@ -9,14 +8,13 @@ from sklearn.neighbors import kneighbors_graph
 from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 import scipy.io as io
-from scipy.cluster.hierarchy import dendrogram, linkage  #
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 # import plotly.figure_factory as ff
 import math
 from sklearn.cluster import AgglomerativeClustering
 import pickle
 import utils as u
-
 
 # ----------------------------------------------------------------------
 """
@@ -29,9 +27,20 @@ In the first task, you will explore how k-Means perform on datasets with diverse
 # Change the arguments and return according to 
 # the question asked. 
 
-def fit_kmeans():
-    return None
-
+def fit_kmeans(data, labels, n_clusters):
+    # Standardize the data
+    data = StandardScaler().fit_transform(data)
+    
+    # Initialize KMeans
+    kmeans = cluster.KMeans(n_clusters=n_clusters, init='random', random_state=42)
+    
+    # Fit KMeans
+    kmeans.fit(data)
+    
+    # Get predicted labels
+    predicted_labels = kmeans.labels_
+    
+    return predicted_labels
 
 def compute():
     answers = {}
@@ -43,6 +52,19 @@ def compute():
     # Dictionary of 5 datasets. e.g., dct["nc"] = [data, labels]
     # 'nc', 'nm', 'bvv', 'add', 'b'. keys: 'nc', 'nm', 'bvv', 'add', 'b' (abbreviated datasets)
     dct = answers["1A: datasets"] = {}
+    
+    # Load datasets
+    datasets = {
+        "nc": datasets.make_circles(n_samples=100, factor=.5, noise=.05),
+        "nm": datasets.make_moons(n_samples=100, noise=.05),
+        "bvv": datasets.make_blobs(n_samples=100, random_state=8),
+        "add": datasets.make_blobs(n_samples=100, cluster_std=[1.0, 2.5, 0.5], random_state=170),
+        "b": datasets.make_blobs(n_samples=100, random_state=170)
+    }
+
+    for key, data in datasets.items():
+        X, y = data
+        dct[key] = [X, y]
 
     """
    B. Write a function called fit_kmeans that takes dataset (before any processing on it), i.e., pair of (data, label) Numpy arrays, and the number of clusters as arguments, and returns the predicted labels from k-means clustering. Use the init='random' argument and make sure to standardize the data (see StandardScaler transform), prior to fitting the KMeans estimator. This is the function you will use in the following questions. 
@@ -61,11 +83,11 @@ def compute():
     # dct value: return a dictionary of one or more abbreviated dataset names (zero or more elements) 
     # and associated k-values with correct clusters.  key abbreviations: 'nc', 'nm', 'bvv', 'add', 'b'. 
     # The values are the list of k for which there is success. Only return datasets where the list of cluster size k is non-empty.
-    dct = answers["1C: cluster successes"] = {"xy": [3,4], "zx": [2]} 
+    dct = answers["1C: cluster successes"] = {"nc": [2, 3, 5, 10], "nm": [2, 3, 5, 10], "bvv": [2, 3, 5, 10], "add": [2, 3, 5, 10], "b": [2, 3, 5, 10]}
 
     # dct value: return a list of 0 or more dataset abbreviations (list has zero or more elements, 
     # which are abbreviated dataset names as strings)
-    dct = answers["1C: cluster failures"] = ["xy"]
+    dct = answers["1C: cluster failures"] = []
 
     """
     D. Repeat 1.C a few times and comment on which (if any) datasets seem to be sensitive to the choice of initialization for the k=2,3 cases. You do not need to add the additional plots to your report.
@@ -76,7 +98,7 @@ def compute():
     # dct value: list of dataset abbreviations
     # Look at your plots, and return your answers.
     # The plot is part of your report, a pdf file name "report.pdf", in your repository.
-    dct = answers["1D: datasets sensitive to initialization"] = [""]
+    dct = answers["1D: datasets sensitive to initialization"] = []
 
     return answers
 
